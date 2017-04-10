668a9a2764b99507ea41d549da57f22d490751a8
rmumi <rijad95@gmail.com>
2017-04-11 00:38:47 +0200

    Publisher and subscriber for Mover4

--- src/checkers_test/src/moving.cpp
+++ src/checkers_test/src/moving.cpp
@@ -1,6 +1,36 @@
 #include "moving.h"
 
-int main(int argc, char** argv()) {
+void joint_states_callback(const sensor_msgs::JointState &msg) {
+     std::cout << msg.position[0] << std::endl;
+}
+
+int main(int argc, char** argv) {
     
+    ros::init(argc, argv, "moving");
+
+    ros::NodeHandle n;
+
+    ros::Subscriber sub = n.subscribe("joint_states", 10, joint_states_callback);
+
+    ros::Publisher pub = n.advertise<sensor_msgs::JointState>("CPRMoverJointVel", 20);
+
+    ros::Rate loop_rate(20);
+
+    while(ros::ok()) {
+        sensor_msgs::JointState pub_vel;
+        pub_vel.header.stamp = ros::Time::now();
+        pub_vel.velocity.resize(4);
+        pub_vel.name.resize(4);
+        for(int i = 0; i < 4; i++) {
+            pub_vel.velocity[i] = 5.0;
+            pub_vel.name[i] = (std::string("Joint") + std::to_string(i+1)).c_str();
+        }
+        pub.publish(pub_vel);
+
+        ros::spinOnce();  // process callbacks
+
+        loop_rate.sleep();
+    }
+
     return 0;
 }
